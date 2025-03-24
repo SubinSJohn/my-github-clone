95c94c137dfb0c05571d11108d067157f299e667
subin <subinsjohn28@gmail.com>
2025-03-24 23:13:50 +0530

    Initial project setup

---
+++ src/main/java/com/subin/github_clone/GithubCloneApplication.java
@@ -0,0 +1,13 @@
+package com.subin.github_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCloneApplication.class, args);
+	}
+
+}
